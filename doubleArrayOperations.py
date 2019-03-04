0b3ed3c1e68ece763415e7b5a84b382598693928
danbergh412 <danbergh412@gmail.com>
2019-03-03 22:28:44 -0800

    Adding double array operations

---
+++ doubleArrayOperations.py
@@ -0,0 +1,13 @@
+arr = [['a', 'b'], ['c', 'd']]
+print(arr[0])
+print(arr[1])
+print(arr[0][0])
+print(arr[0][1])
+print(arr[1][0])
+print(arr[1][1])
+
+
+print("\nPrinting Array")
+for i in range(2):
+	for j in range(2):
+		print(arr[i][j])
